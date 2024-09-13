4d8b86a4a9048651a566308e8028202d6d26c536
StefanyLopez <141768826+StefanyLopez@users.noreply.github.com>
2024-09-13 13:01:40 -0500

    Cambio del banner con boton

--- Assets/JavaScript/Filter.js
+++ Assets/JavaScript/Filter.js
@@ -1,6 +1,21 @@
 // Select all resources buttons and resources cards
 const resourcesNav = document.querySelectorAll(".Resources-nav button");
 const resourcesCards = document.querySelectorAll(".Resources-cards .card");
+const bannerResources = document.getElementById('banner-resources');
+const bannerYoutube = document.getElementById('banner-youtube');
+const bannerInteractivity = document.getElementById('banner-interactivity');
+const bannerSpline = document.getElementById('banner-spline');
+const bannerOthers = document.getElementById('banner-others');
+
+
+function reloadSplineViewer(banner) {
+    const splineViewer = banner.querySelector('spline-viewer');
+    if (splineViewer) {
+        const newViewer = document.createElement('spline-viewer');
+        newViewer.setAttribute('url', splineViewer.getAttribute('url'));
+        banner.replaceChild(newViewer, splineViewer); 
+    }
+}
 
 // Add click event listener to each button
 resourcesNav.forEach(button => {
@@ -20,8 +35,35 @@ resourcesNav.forEach(button => {
                 card.classList.add("hide");
             }
         });
+
+        bannerResources.style.display = 'none';
+        bannerYoutube.style.display = 'none';
+        bannerInteractivity.style.display = 'none';
+        bannerSpline.style.display = 'none';
+        bannerOthers.style.display = 'none';
+
+
+
+        if (category === "youtube") {
+            bannerYoutube.style.display = 'block';
+            reloadSplineViewer(bannerYoutube);
+        } else if (category === "Interactivity") {
+            bannerInteractivity.style.display = 'block';
+            reloadSplineViewer(bannerInteractivity);
+        } else if (category === "spline") {
+            bannerSpline.style.display = 'block';
+            reloadSplineViewer(bannerSpline);
+        } else if (category === "others") {
+            bannerOthers.style.display = 'block';
+            reloadSplineViewer(bannerOthers);
+        } else {
+            bannerResources.style.display = 'block';
+            reloadSplineViewer(bannerResources);
+        }
+
     });
 });
+
 
 
 document.addEventListener('DOMContentLoaded', function () {
